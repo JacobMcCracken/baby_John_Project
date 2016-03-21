a8b9e4557f6c82d08ff2f7fcfde0780a01076223
root <root@localhost.localdomain>
2016-03-21 19:38:21 -0400

    Incremental finished, added default .conf file and test .conf file

--- baby_John.sh
+++ baby_John.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 #############################################################################################
-echo "Please select a mode or other options."
+#Variables
+defaultconf="/home/jacob/baby_John_Project/default_john.conf"
+confPATH="/home/jacob/baby_John_Project/test_john.conf"
 OPTIONS="Single Incremental Wordlist Extrenal OtherOptions Quit"
-IOPTIONS="ASCII LM_ASCII ALPHANUMERIC ALPHA_CHARS LOWERNUM UPPERNUM LOWERSPACE  "
+
+IOPTIONS="ASCII LM_ASCII Alnum Alpha LowerNum UpperNum LowerSpace Lower Upper Digits Info"
+ASCII="ascii.chr\nMinLen = 0\nMaxLen = 13"
+LM_ASCII="lm_ascii.chr\nMinLen = 0\nMaxLen = 7"
+Alnum="alnum.chr\nMinLen = 1\nMaxLen = 13"
+Alpha="alpha.chr\nMinLen = 1\nMaxLen = 13"
+LowerNum="lowernum.chr\nMinLen = 1\nMaxLen = 13"
+UpperNum="uppernum.chr\nMinLen = 1\nMaxLen = 13"
+LowerSpace="lowerspace.chr\nMinLen = 1\nMaxLen = 13"
+Lower="lower.chr\nMinLen = 1\nMaxLen = 13"
+Upper="upper.chr\nMinLen = 1\nMaxLen = 13"
+Digits="digits.chr\nMinLen = 1\nMaxLen = 20"
+
+#############################################################################################
+
+cat $defaultconf > $confPATH
+echo "Please select a mode or other options."
 select opt in $OPTIONS; do
 
 #############################################################################################
@@ -26,17 +44,108 @@ select opt in $OPTIONS; do
 		echo "Please enter the username of the account you would like to crack"
 		echo "or enter ALL to attempt to crack all accounts in the target file"
 		read account
-		echo "Please select an inciemental mode."
-		select opt in $IOPTIONS; do
-		if [ $account == "ALL" [; then
-			echo "Please enter the target for the single crack"
-
-		else
-			echo "Please enter the target for the single crack"
-			read target
-			
-		fi
-
+		echo "Please enter the minimum length of the password to be cracked."
+		read min
+		echo "Please enter the maximum length of the password to be cracked."
+		read max
+		echo "Please enter the target for the single crack"
+		read target
+		echo "Please select an inciemental mode or choose Info for information"
+		echo "on each mode."
+		select iopt in $IOPTIONS; do
+			if [ "$iopt" = "ASCII" ]; then
+				sed -i "s/MinLen = 0/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=ascii $target
+				else
+					john --incremental=ascii -u=$account $target
+				fi
+			elif [ "$iopt" = "LM_ASCII" ]; then
+				sed -i "s/MinLen = 0/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 7/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=lm_ascii $target
+				else
+					john --incremental=lm_ascii -u=$account $target
+				fi
+			elif [ "$iopt" = "Alnum" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=alnum $target
+				else
+					john --incremental=alnum -u=$account $target
+				fi
+			elif [ "$iopt" = "Alpha" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=alpha $target
+				else
+					john --incremental=alpha -u=$account $target
+				fi
+			elif [ "$iopt" = "LowerNum" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=lowernum $target
+				else
+					john --incremental=lowernum -u=$account $target
+				fi
+			elif [ "$iopt" = "UpperNum" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=uppernum $target
+				else
+					john --incremental=uppernum -u=$account $target
+				fi
+			elif [ "$iopt" = "LowerSpace" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=lowerspace $target
+				else
+					john --incremental=lowerspace -u=$account $target
+				fi
+			elif [ "$iopt" = "Lower" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=lower $target
+				else
+					john --incremental=lower -u=$account $target
+				fi
+			elif [ "$iopt" = "Upper" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 13/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=upper $target
+				else
+					john --incremental=upper -u=$account $target
+				fi
+			elif [ "$iopt" = "Digits" ]; then
+				sed -i "s/MinLen = 1/MinLen = $min/g" $confPATH
+				sed -i "s/MaxLen = 20/MaxLen = $max/g" $confPATH
+				if [ "$account" = "ALL" ]; then
+					john --incremental=digits $target
+				else
+					john --incremental=digits -u=$account $target
+				fi
+			elif [ "$iopt" = "Info" ]; then
+				echo "ASCII - uses all 95 printable ASCII characters"
+				echo "LM_ASCII - same as ASCII, used on LM hashes "
+				echo "Alnum - uses all 62 alphanumeric characters"
+				echo "Alpha - uses all upper and lower case letters"
+				echo "LowerNum - uses lowercase letters and digits"
+				echo "UpperNum - uses uppercase letters and digits"
+				echo "LowerSpace - uses lowercase letters and spaces"
+				echo "Lower - uses only all lowercase letters"
+				echo "Upper - uses only all uppercase letters"
+				echo "Digits - uses only all digits (0-9)"
+			fi
+		done
 #############################################################################################
 #Wordlist
 	elif [ "$opt" = "Wordlist" ]; then
